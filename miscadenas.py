82a3ce91b47df2f181a1730f0d787d71d09f5d95
Roberto Gaytan Mendoza <a.22308051281202@cbtis128.edu.mx>
2024-09-06 09:33:23 -0600

    Add files via upload
    Subi los archivos

---
+++ miscadenas.py
@@ -0,0 +1,23 @@
+nombre="Roberto"
+print(nombre[0])
+print(nombre)
+for c in "Roberto":
+  print(c)
+
+
+MisNovias=["A" , "r" , "i"]
+for chicas in MisNovias:
+  print(chicas)
+
+print("Otro ejemplo")
+
+b = "Roberto Gaytan"
+print(b[2:5])
+print("A MAYUSCULAS")
+a="eliseo nava"
+print(a.upper())
+print("CONCATENAR")
+a= "Maria"
+b="Perez"
+c=a+" "+b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,16 @@
+import math
+edad=16  # 16 se guarda edad
+
+nombre="Ariel Rodriguez"
+print("tu edad es",edad)
+print("te llamas",nombre)
+print("--tipo de variables--")
+x = str(7)    # x will be '3'
+y = int(3.1416)    # y will be 3
+z = float(7)
+print(x)
+print(y)
+print(z)
+print("--FUNCIONES MATEMATICAS--")
+x = math.sqrt(64)
+print(x)
